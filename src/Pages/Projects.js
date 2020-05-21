005b9384f735fb30b272776196c9b164fef17ed8
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-05-21 15:52:01 +0100

    Added Kodflix project image and rounded edges

--- src/Pages/Projects.js
+++ src/Pages/Projects.js
@@ -1,10 +1,17 @@
 import React from "react";
+import kodflixLogo from "../Images/kodflixLogo.png";
+import { Link } from "react-router-dom";
 
 export default function Projects() {
   return (
     <div className="projects-container">
       <h1 className="projects-title">Projects.</h1>
-      <div className="projects-text"></div>
+      <div className="projects-grid">
+        <img className="project-image" src={kodflixLogo} alt="Project Logo" />
+        <div className="overlay">
+          <div className="project-text-box">Kodflix</div>
+        </div>
+      </div>
     </div>
   );
 }
