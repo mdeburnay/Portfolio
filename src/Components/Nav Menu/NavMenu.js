703a78b564bcc079ad663f29bf1d850bdf80a1ff
MdBB <48160441+maximoose773@users.noreply.github.com>
2020-09-14 09:33:11 +0100

    made changes to CSS on all pages. Fixed bug with Navbar logo

--- src/Components/Nav Menu/NavMenu.js
+++ src/Components/Nav Menu/NavMenu.js
@@ -6,16 +6,12 @@ import "./NavMenu.css";
 export default function NavMenu() {
   return (
     <nav className="navbar-content">
-      <div className="navbar-logo-container">
-        <Link className="nav-link logo" smooth to="#home">
-          MDB<span className="fullstop">.</span>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap"
-            rel="stylesheet"
-          ></link>
-        </Link>
-      </div>
       <div className="navbar-link-container">
+        <div className="navbar-logo-container">
+          <Link className="nav-link logo" smooth to="#home">
+            MDB<span className="fullstop">.</span>
+          </Link>
+        </div>
         <Link className="nav-link" smooth to="#about">
           About
         </Link>
